baccf6f7ff715b7d0e29187ecf0e0c58cd73b8e2
humberto <humberto_molinalopez@hotmail.com>
2023-02-20 15:53:28 -0600

    añadiendo helper para validar una url que sea valida con varios niveles de comprovacion

---
+++ PriceTrendCam.Core/Helpers/Url.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+namespace PriceTrendCam.Core.Helpers;
+public class Url
+{
+    public static async Task<bool> IsValid(string url)
+    {
+        if (string.IsNullOrEmpty(url)) { return false; }
+
+        // Comprobar si la URL es válida según la sintaxis de una URL
+        var regex = new Regex(@"^(https?://)?([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?$");
+
+        if (!regex.IsMatch(url)) { return false; }
+
+        try
+        {
+            using var client = new HttpClient();
+            // Enviar una solicitud HTTP GET a la URL
+            var response = await client.GetAsync(url);
+
+            // Comprobar si la respuesta tiene un código de estado válido
+            if (response.StatusCode == HttpStatusCode.OK) { return true; }
+        }
+        catch (HttpRequestException)
+        {
+            // Si se produce una excepción al enviar la solicitud HTTP, la URL no es válida
+        }
+
+        return false;
+    }
+}
