8b95d14c4864e7b4ba16a7fcc030a88a0e12a976
humberto <humberto_molinalopez@hotmail.com>
2023-02-20 12:38:36 -0600

    añadiendo cuadro de busqueda a pagina main

--- PriceTrendCam/Views/MainPage.xaml.cs
+++ PriceTrendCam/Views/MainPage.xaml.cs
@@ -16,4 +16,13 @@ public sealed partial class MainPage : Page
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
     }
+
+    private void Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        //obtenemos todos los url de busqueda de productos
+        //hacemos condicional para averiguar si es una url valida
+        //si es una url valida obtendremos su store y selectores
+        //si es una url que tiene varios productos de terceros avisaremos al usuario de si quiere añadir envio masivo de urls
+        // si no continuamos con el registro comun de los productos
+    }
 }
