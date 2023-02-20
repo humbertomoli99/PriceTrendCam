707a1baf00b7bf24cd0f348f31d9577dc134a1a3
humberto <humberto_molinalopez@hotmail.com>
2023-02-20 13:43:04 -0600

    Añadiendo controls correctamente para winui3

---
+++ PriceTrendCam/Controls/DocumentationBlock.cs
@@ -0,0 +1,95 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.UI.Animations;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm;
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using CommunityToolkit.WinUI.UI.Controls;
+using Windows.System;
+
+#nullable enable
+
+namespace PriceTrendCam.Controls;
+
+/// <summary>
+/// A simple control that acts as a container for a documentation block.
+/// </summary>
+[TemplatePart(Name = "PART_MarkdownTextBlock", Type = typeof(MarkdownTextBlock))]
+public sealed class DocumentationBlock : ContentControl
+{
+    /// <summary>
+    /// Creates a new <see cref="DocumentationBlock"/> instance.
+    /// </summary>
+    public DocumentationBlock()
+    {
+        Implicit.SetAnimations(this, new ImplicitAnimationSet { new OffsetAnimation() });
+    }
+
+    /// <summary>
+    /// The <see cref="MarkdownTextBlock"/> instance in use.
+    /// </summary>
+    private MarkdownTextBlock? markdownTextBlock;
+
+    /// <inheritdoc/>
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+
+        markdownTextBlock = (MarkdownTextBlock)GetTemplateChild("PART_MarkdownTextBlock")!;
+
+        markdownTextBlock.LinkClicked += MarkdownTextBlock_LinkClicked;
+        markdownTextBlock.ImageResolving += MarkdownTextBlock_ImageResolving;
+    }
+
+    /// <summary>
+    /// Handles a clicked link in a markdown block.
+    /// </summary>
+    /// <param name="sender">The source <see cref="MarkdownTextBlock"/> control.</param>
+    /// <param name="e">The input arguments.</param>
+    private void MarkdownTextBlock_LinkClicked(object sender, LinkClickedEventArgs e)
+    {
+        if (Uri.TryCreate(e.Link, UriKind.Absolute, out Uri result) ||
+            (e.Link.StartsWith("/dotnet") &&
+             Uri.TryCreate($"https://docs.microsoft.com{e.Link}", UriKind.Absolute, out result)))
+        {
+            _ = Launcher.LaunchUriAsync(result);
+        }
+    }
+
+    /// <summary>
+    /// Handles an image being resolved and loads images from the local assets.
+    /// </summary>
+    /// <param name="sender">The source <see cref="MarkdownTextBlock"/> control.</param>
+    /// <param name="e">The input arguments.</param>
+    private void MarkdownTextBlock_ImageResolving(object sender, ImageResolvingEventArgs e)
+    {
+        if (e.Url?.Contains("source-generator-visualization.png") == true)
+        {
+            e.Image = new BitmapImage(new Uri("ms-appx:///Assets/docs/images/source-generator-visualization.png"));
+            e.Handled = true;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="string"/> representing the text to display.
+    /// </summary>
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    /// <summary>
+    /// The <see cref="DependencyProperty"/> backing <see cref="Text"/>.
+    /// </summary>
+    public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+        nameof(Text),
+        typeof(string),
+        typeof(DocumentationBlock),
+        new PropertyMetadata(default(string)));
+}
---
+++ PriceTrendCam/Controls/InteractiveSample.cs
@@ -0,0 +1,65 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CommunityToolkit.WinUI.UI.Animations;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm;
+using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using CommunityToolkit.WinUI.UI.Controls;
+using Windows.System;
+
+namespace PriceTrendCam.Controls;
+
+/// <summary>
+/// A simple control that acts as a frame for an interactive sample.
+/// </summary>
+public sealed class InteractiveSample : ContentControl
+{
+    /// <summary>
+    /// Creates a new <see cref="InteractiveSample"/> instance.
+    /// </summary>
+    public InteractiveSample()
+    {
+        Implicit.SetAnimations(this, new ImplicitAnimationSet { new OffsetAnimation() });
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="string"/> representing the C# code to display.
+    /// </summary>
+    public string CSharpCode
+    {
+        get => (string)GetValue(CSharpCodeProperty);
+        set => SetValue(CSharpCodeProperty, $"```csharp\n{value.Trim()}\n```");
+    }
+
+    /// <summary>
+    /// The <see cref="DependencyProperty"/> backing <see cref="CSharpCode"/>.
+    /// </summary>
+    public static readonly DependencyProperty CSharpCodeProperty = DependencyProperty.Register(
+        nameof(CSharpCode),
+        typeof(string),
+        typeof(InteractiveSample),
+        new PropertyMetadata(default(string)));
+
+    /// <summary>
+    /// Gets or sets the <see cref="string"/> representing the XAML code to display.
+    /// </summary>
+    public string XamlCode
+    {
+        get => (string)GetValue(XamlCodeProperty);
+        set => SetValue(XamlCodeProperty, $"```xml\n{value.Trim()}\n```");
+    }
+
+    /// <summary>
+    /// The <see cref="DependencyProperty"/> backing <see cref="CSharpCode"/>.
+    /// </summary>
+    public static readonly DependencyProperty XamlCodeProperty = DependencyProperty.Register(
+        nameof(XamlCode),
+        typeof(string),
+        typeof(InteractiveSample),
+        new PropertyMetadata(default(string)));
+}
