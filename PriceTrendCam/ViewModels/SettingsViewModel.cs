3e13c29018becc974d4e40f9d03c89360ea6ba48
humberto <humberto_molinalopez@hotmail.com>
2023-03-07 00:13:07 -0600

    usando ClipboardSelectorService en la pagina de Settings

--- PriceTrendCam/ViewModels/SettingsViewModel.cs
+++ PriceTrendCam/ViewModels/SettingsViewModel.cs
@@ -5,19 +5,20 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 using Microsoft.UI.Xaml;
-
+using Microsoft.UI.Xaml.Controls;
 using PriceTrendCam.Contracts.Services;
 using PriceTrendCam.Helpers;
-
+using PriceTrendCam.Services;
 using Windows.ApplicationModel;
 
 namespace PriceTrendCam.ViewModels;
 
-public class SettingsViewModel : ObservableRecipient
+public partial class SettingsViewModel : ObservableRecipient
 {
     private readonly IThemeSelectorService _themeSelectorService;
     private ElementTheme _elementTheme;
     private string _versionDescription;
+    private readonly IClipboardSelectorService _clipboardSelectorService;
 
     public ElementTheme ElementTheme
     {
@@ -36,11 +37,21 @@ public class SettingsViewModel : ObservableRecipient
         get;
     }
 
-    public SettingsViewModel(IThemeSelectorService themeSelectorService)
+    public bool ClipboardAutomatically
     {
+        get;set;
+    }
+    
+    public SettingsViewModel(IThemeSelectorService themeSelectorService, IClipboardSelectorService clipboardSelectorService)
+    {
+        _clipboardSelectorService = clipboardSelectorService;
+
         _themeSelectorService = themeSelectorService;
+
         _elementTheme = _themeSelectorService.Theme;
         _versionDescription = GetVersionDescription();
+
+        _ = GetBoolClipboard();
 
         SwitchThemeCommand = new RelayCommand<ElementTheme>(
             async (param) =>
@@ -51,6 +62,11 @@ public class SettingsViewModel : ObservableRecipient
                     await _themeSelectorService.SetThemeAsync(param);
                 }
             });
+    }
+
+    private async Task GetBoolClipboard()
+    {
+        ClipboardAutomatically = await _clipboardSelectorService.LoadClipboardSettingFromSettingsAsync();
     }
 
     private static string GetVersionDescription()
@@ -69,5 +85,13 @@ public class SettingsViewModel : ObservableRecipient
         }
 
         return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+    }
+    public async void OnToggleSwitchToggled(object sender, RoutedEventArgs e)
+    {
+        // Se cambia el valor de ClipboardAutomatically según si está activado o no el ToggleSwitch
+        ClipboardAutomatically = ((ToggleSwitch)sender).IsOn;
+
+        // Se guarda el valor en localSettings
+        await _clipboardSelectorService.SaveClipboardSettingInSettingsAsync(ClipboardAutomatically).ConfigureAwait(false);
     }
 }
