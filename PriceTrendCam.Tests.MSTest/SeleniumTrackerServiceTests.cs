ab46e60e8ed06caac7cb9938cbf7604cf2f90fc4
humberto <humberto_molinalopez@hotmail.com>
2023-03-09 12:43:53 -0600

    añadiendo pruebas unitarias a selenium

---
+++ PriceTrendCam.Tests.MSTest/SeleniumTrackerServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using PriceTrendCam.Core.Services;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
+
+namespace PriceTrendCam.Tests.MSTest;
+
+[TestClass]
+public class SeleniumTrackerServiceTests
+{
+    private static EdgeDriver driver;
+
+    [ClassInitialize]
+    public static void Setup(TestContext context)
+    {
+
+    }
+
+    [TestMethod]
+    public void ExecuteScript_ShouldReturnNonNullObject()
+    {
+        // Arrange
+        string script = "document.title;";
+
+        // Act
+        string result = SeleniumTrackerService.GetGooglePageTitle();
+
+        // Assert
+        Assert.IsNotNull(result);
+    }
+
+    [ClassCleanup]
+    public static void Cleanup()
+    {
+
+    }
+}
