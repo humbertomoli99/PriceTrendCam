3d01b269d593206c46c233861ec1c09e8234be7b
humberto <humberto_molinalopez@hotmail.com>
2023-03-11 13:55:51 -0600

    creando servicio para ejecutar todos los scripts de un sitio web js con Jint

---
+++ PriceTrendCam.Core/Services/ScriptExecutorService.cs
@@ -0,0 +1,102 @@
+﻿using HtmlAgilityPack;
+using Jint;
+
+namespace PriceTrendCam.Core.Services;
+public class ScriptExecutorService
+{
+    public static void Main(string[] args)
+    {
+        // Get the HTML of the website
+        var html = GetHtmlFromSite("https://www.example.com").Result;
+
+        // Create an HTML document and load the website's HTML
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+
+        // Get the root node of the HTML document
+        var siteNode = GetRootOfHtml(doc);
+
+        // Execute the website's scripts
+        ExecuteSiteScripts(siteNode).Wait();
+    }
+
+    public static async Task<string> GetHtmlFromSite(string url)
+    {
+        // Create an HTTP client and get the website's HTML
+        using (var httpClient = new System.Net.Http.HttpClient())
+        {
+            var response = await httpClient.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+    public static HtmlNode GetRootOfHtml(HtmlDocument doc)
+    {
+        return doc.DocumentNode;
+    }
+
+    public static List<string> GetScriptsFromHtmlNode(HtmlNode node)
+    {
+        var scripts = new List<string>();
+        // Buscar nodos de script y agregar el contenido de cada nodo a la lista de scripts
+        foreach (var scriptNode in node.Descendants("script"))
+        {
+            if (!string.IsNullOrEmpty(scriptNode.InnerText))
+            {
+                scripts.Add(scriptNode.InnerText.Trim());
+            }
+        }
+
+        return scripts;
+    }
+
+    public static async Task ExecuteSiteScripts(HtmlNode siteNode)
+    {
+        // Get all scripts from the site
+        var scripts = GetScriptsFromHtmlNode(siteNode);
+
+        // Create an instance of the Jint engine
+        var engine = new Engine();
+
+        // Execute each script in order
+        foreach (var script in scripts)
+        {
+            await ExecuteScript(script, engine);
+        }
+    }
+
+    public static async Task ExecuteScript(string script, Engine engine)
+    {
+        // Provide a custom implementation of the console object to Jint
+        engine.SetValue("console", new CustomConsole());
+
+        if (script.StartsWith("<script") && script.Contains("src="))
+        {
+            // The script is loaded from an external URL
+            var startIndex = script.IndexOf("src=") + 5;
+            var endIndex = script.IndexOf("\"", startIndex);
+            var url = script.Substring(startIndex, endIndex - startIndex);
+
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                var content = await response.Content.ReadAsStringAsync();
+                engine.Execute(content);
+            }
+        }
+        else
+        {
+            // The script is contained within the HTML page
+            script = script.Replace("<script>", "").Replace("</script>", "");
+            engine.Execute(script);
+        }
+    }
+
+    public class CustomConsole
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ PriceTrendCam.Tests.MSTest/ScriptExecutorServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using HtmlAgilityPack;
+using Jint;
+using PriceTrendCam.Core.Services;
+
+namespace PriceTrendCam.Tests.MSTest;
+[TestClass]
+public class ScriptExecutorServiceTests
+{
+    [TestMethod]
+    public async Task GetHtmlFromSite_ReturnsNonEmptyString()
+    {
+        // Arrange
+        var url = "https://www.example.com";
+
+        // Act
+        var html = await ScriptExecutorService.GetHtmlFromSite(url);
+
+        // Assert
+        Assert.IsFalse(string.IsNullOrEmpty(html));
+    }
+
+    [TestMethod]
+    public void GetRootOfHtml_ReturnsRootNode()
+    {
+        // Arrange
+        var doc = new HtmlDocument();
+        doc.LoadHtml("<html><head></head><body></body></html>");
+
+        // Act
+        var rootNode = ScriptExecutorService.GetRootOfHtml(doc);
+
+        // Assert
+        Assert.AreEqual("#document", rootNode.Name);
+    }
+
+    [TestMethod]
+    public void GetScriptsFromHtmlNode_ReturnsListOfScripts()
+    {
+        // Arrange
+        var html = "<html><head></head><body><script>console.log('Hello, world!');</script></body></html>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var rootNode = ScriptExecutorService.GetRootOfHtml(doc);
+
+        // Act
+        var scripts = ScriptExecutorService.GetScriptsFromHtmlNode(rootNode);
+
+        // Assert
+        Assert.AreEqual(1, scripts.Count);
+        Assert.AreEqual("console.log('Hello, world!');", scripts[0]);
+    }
+
+    [TestMethod]
+    public async Task ExecuteScript_ExecutesScript()
+    {
+        // Arrange
+        var engine = new Engine();
+        var script = "console.log('Hello, world!');";
+
+        // Act
+        await ScriptExecutorService.ExecuteScript(script, engine);
+
+        // Assert
+        // We can't actually test the console output, but we can check that the script ran without errors
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public async Task ExecuteSiteScripts_ExecutesAllScripts()
+    {
+        // Arrange
+        var html = "<html><head></head><body><script>console.log('Hello, world!');</script><script>console.log('Goodbye, world!');</script></body></html>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var rootNode = ScriptExecutorService.GetRootOfHtml(doc);
+
+        // Act
+        await ScriptExecutorService.ExecuteSiteScripts(rootNode);
+
+        // Assert
+        // We can't actually test the console output, but we can check that both scripts ran without errors
+        Assert.IsTrue(true);
+    }
+
+    [TestMethod]
+    public async Task ExecuteScript_ExecutesExternalScript()
+    {
+        // Arrange
+        string script = "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js\"></script>";
+        Engine engine = new Engine();
+
+        // Act
+        await ScriptExecutorService.ExecuteScript(script, engine);
+
+        // Assert
+        Assert.IsNotNull(engine.GetValue("moment"));
+    }
+}
