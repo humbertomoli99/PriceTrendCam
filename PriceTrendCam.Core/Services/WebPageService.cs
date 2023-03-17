ee32d7702f70ddaab0493a432349a8d69a2950cd
humberto <humberto_molinalopez@hotmail.com>
2023-03-16 21:25:37 -0600

    Creando servicio para web scraping con anglesharp y pruebas unitarias

---
+++ PriceTrendCam.Core/Services/WebPageService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Io;
+using OpenQA.Selenium.DevTools;
+using System.Net.Http.Headers;
+
+namespace PriceTrendCam.Core.Services;
+public class WebPageService
+{
+    private HttpClient _client;
+    private IConfiguration _config;
+    private IElement _pageContent;
+
+    public WebPageService()
+    {
+        _config = Configuration.Default.WithDefaultLoader();
+
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.41");
+        _client = client;
+    }
+
+    public async Task<IElement> LoadPageAsync(string RequestUri)
+    {
+        var context = BrowsingContext.New(_config);
+        var response = await _client.GetAsync(RequestUri);
+        var content = await response.Content.ReadAsStringAsync();
+
+        var document = await context.OpenAsync(req => req.Content(content));
+        _pageContent = document.DocumentElement;
+        return _pageContent;
+    }
+
+    public IElement SelectElement(string selector)
+    {
+        return _pageContent.QuerySelector(selector);
+    }
+
+    public string GetAttributeValue(IElement element, string attributeName)
+    {
+        return element.GetAttribute(attributeName);
+    }
+}
---
+++ PriceTrendCam.Tests.MSTest/WebPageServiceTests.cs
@@ -0,0 +1,63 @@
+﻿using PriceTrendCam.Core.Services;
+
+[TestClass]
+public class WebPageServiceTests
+{
+    private WebPageService _webPageService;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _webPageService = new WebPageService();
+    }
+
+    [TestMethod]
+    public async Task LoadPage_ShouldLoadPageContent()
+    {
+        // Arrange
+        var url = "https://www.google.com.mx/";
+
+        // Act
+        var pageContent = await _webPageService.LoadPageAsync(url);
+
+        // Assert
+        Assert.IsNotNull(pageContent);
+        Assert.IsTrue(pageContent.ChildElementCount > 0);
+    }
+
+    [TestMethod]
+    public async Task SelectElement_ShouldReturnCorrectElement()
+    {
+        // Arrange
+        var selector = "body > div.L3eUgb > div.o3j99.c93Gbe > div.uU7dJb";
+        var expectedText = "México";
+        var url = "https://www.google.com.mx/";
+
+        // Act
+        await _webPageService.LoadPageAsync(url);
+        var selectedElement = _webPageService.SelectElement(selector);
+
+        // Assert
+        Assert.IsNotNull(selectedElement);
+        Assert.AreEqual(expectedText, selectedElement.InnerHtml);
+    }
+
+    [TestMethod]
+    public async Task GetAttributeValue_ShouldReturnCorrectValue()
+    {
+        // Arrange
+        var selector = "body > div.L3eUgb > div.o3j99.c93Gbe > div.KxwPGc.SSwjIe > div.KxwPGc.AghGtd > a:nth-child(1)";
+        var attributeName = "href";
+        var expectedValue = "https://about.google/?utm_source=google-MX&utm_medium=referral&utm_campaign=hp-footer&fg=1";
+        var url = "https://www.google.com.mx/";
+
+        // Act
+        await _webPageService.LoadPageAsync(url);
+        var selectedElement = _webPageService.SelectElement(selector);
+        var attributeValue = _webPageService.GetAttributeValue(selectedElement, attributeName);
+
+        // Assert
+        Assert.IsNotNull(selectedElement);
+        Assert.AreEqual(expectedValue, attributeValue);
+    }
+}
