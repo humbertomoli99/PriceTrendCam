ed58aa98ab8373243a1858f8760cd1382b8d3bc6
humberto <humberto_molinalopez@hotmail.com>
2023-03-18 15:08:13 -0600

    borrando using cache ya q genera problemas

--- PriceTrendCam.Core/Services/HtmlDocumentService.cs
+++ PriceTrendCam.Core/Services/HtmlDocumentService.cs
@@ -20,7 +20,6 @@ public class HtmlDocumentService
         HtmlWeb web = new HtmlWeb();
         web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
         web.AutomaticDecompression = DecompressionMethods.GZip;
-        web.UsingCache = true;
         web.UseCookies = true;
 
         var htmlDoc = web.Load(RequestUri);
