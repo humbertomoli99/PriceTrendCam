09c5221bf798bf9e5ca766e02c27f54a8337b03d
humberto <humberto_molinalopez@hotmail.com>
2023-03-23 00:31:01 -0600

    añadiendo configuraciones adicionales de htmlweb a metodo de LoadPageAsync

--- PriceTrendCam.Core/Services/HtmlDocumentService.cs
+++ PriceTrendCam.Core/Services/HtmlDocumentService.cs
@@ -21,6 +21,9 @@ public class HtmlDocumentService
         web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3";
         web.AutomaticDecompression = DecompressionMethods.GZip;
         web.UseCookies = true;
+        web.AutoDetectEncoding = true;
+        web.OverrideEncoding = System.Text.Encoding.UTF8;
+        web.UsingCacheIfExists = true;
 
         var htmlDoc = web.Load(RequestUri);
 
