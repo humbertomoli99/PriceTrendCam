5f26206c6d226c7b2f2c9ba35a217bd31373fe79
humberto <humberto_molinalopez@hotmail.com>
2023-04-09 17:05:43 -0600

    Mostrando lista de productos en main page

--- PriceTrendCam/Views/MainPage.xaml.cs
+++ PriceTrendCam/Views/MainPage.xaml.cs
@@ -1,10 +1,41 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using System.Collections.ObjectModel;
+using Microsoft.UI.Xaml.Controls;
+using PriceTrendCam.Core.Models;
 using PriceTrendCam.ViewModels;
 
 namespace PriceTrendCam.Views;
 
+public class ListItemData2
+{
+    public int Id
+    {
+        get; set;
+    }
+    public string Title
+    {
+        get; set;
+    }
+    public string Description
+    {
+        get; set;
+    }
+    public string ImageLocation
+    {
+        get; set;
+    }
+    public string Price
+    {
+        get; set;
+    }
+    public string Stock
+    {
+        get; set;
+    }
+}
 public sealed partial class MainPage : Page
 {
+    readonly ObservableCollection<ListItemData2> collection = new();
+
     public MainViewModel ViewModel
     {
         get;
@@ -16,10 +47,29 @@ public sealed partial class MainPage : Page
         InitializeComponent();
         Loaded += MainPage_Loaded;
     }
+    public void LoadProductsIntoList(List<ProductInfo> Products)
+    {
+        collection.Clear();
+        foreach (var item in Products)
+        {
+            var listProductsItem = new ListItemData2
+            {
+                Id = item.Id,
+                Title = item.Name,
+                Description = item.Description,
+                ImageLocation = item.Image,
+                Price = item.Price.ToString(),
+                Stock = item.Stock.ToString()
+            };
 
-    private void MainPage_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+            collection.Add(listProductsItem);
+        }
+    }
+    private async void MainPage_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
         ViewModel.XamlRoot = this.XamlRoot;
+        var products = await App.PriceTrackerService.GetAllWithChildrenAsync<ProductInfo>();
+        LoadProductsIntoList(products);
     }
 
     private async void Page_GotFocus(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
