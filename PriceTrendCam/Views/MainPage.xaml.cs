7f5d03601e73ed6cf22da1c5bed533344a194e7f
humberto <humberto_molinalopez@hotmail.com>
2023-04-13 05:33:38 -0600

    resolviendo error de carga de listview al iniciar la app

--- PriceTrendCam/Views/MainPage.xaml.cs
+++ PriceTrendCam/Views/MainPage.xaml.cs
@@ -75,8 +75,11 @@ public sealed partial class MainPage : Page
         // Cargar los productos en la lista
         await ViewModel.LoadProductsAsync();
 
+        // Obtener instancia del view model
+        MainViewModel viewModel = new MainViewModel();
+
         // Establecer el contexto de datos del ListView
-        DataContext = ViewModel;
+        DataContext = viewModel;
     }
 
     private async void Page_GotFocus(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
