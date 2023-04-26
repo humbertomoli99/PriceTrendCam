f81fcc533849fd3b7b4687106149500092a5b46d
humberto <humberto_molinalopez@hotmail.com>
2023-04-26 11:57:54 -0600

    cambiando metodo a no asincrono App_UnhandledException

--- PriceTrendCam/App.xaml.cs
+++ PriceTrendCam/App.xaml.cs
@@ -134,7 +134,7 @@ public partial class App : Application
         UnhandledException += App_UnhandledException;
     }
 
-    private async void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
     {
         // TODO: Log and handle exceptions as appropriate.
         // https://docs.microsoft.com/windows/windows-app-sdk/api/winrt/microsoft.ui.xaml.application.unhandledexception.
