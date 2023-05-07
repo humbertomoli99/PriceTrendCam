4b454725314f283fe7ee4d54fda5083b01ea3c35
humberto <humberto_molinalopez@hotmail.com>
2023-05-07 07:18:06 -0600

    añadiendo mainmodel para reducir el codigo en el viewmodel de main para usar el patron mvvm

--- PriceTrendCam/Models/MainModel.cs
+++ PriceTrendCam/Models/MainModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
