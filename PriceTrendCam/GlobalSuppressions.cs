0cbfc7cfcc1a491888feb2e6bda15ca4b5a15c9d
humberto <humberto_molinalopez@hotmail.com>
2023-05-07 15:12:32 -0600

    Suprimiendo errores de IDE0044 class ProductListItem

---
+++ PriceTrendCam/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0044:Agregar modificador de solo lectura", Justification = "<pendiente>", Scope = "member", Target = "~F:PriceTrendCam.Models.ProductListItem.title")]
+[assembly: SuppressMessage("Style", "IDE0044:Agregar modificador de solo lectura", Justification = "<pendiente>", Scope = "member", Target = "~F:PriceTrendCam.Models.ProductListItem.id")]
+[assembly: SuppressMessage("Style", "IDE0044:Agregar modificador de solo lectura", Justification = "<pendiente>", Scope = "member", Target = "~F:PriceTrendCam.Models.ProductListItem.imageLocation")]
+[assembly: SuppressMessage("Style", "IDE0044:Agregar modificador de solo lectura", Justification = "<pendiente>", Scope = "member", Target = "~F:PriceTrendCam.Models.ProductListItem.price")]
+[assembly: SuppressMessage("Style", "IDE0044:Agregar modificador de solo lectura", Justification = "<pendiente>", Scope = "member", Target = "~F:PriceTrendCam.Models.ProductListItem.shipping")]
+[assembly: SuppressMessage("Style", "IDE0044:Agregar modificador de solo lectura", Justification = "<pendiente>", Scope = "member", Target = "~F:PriceTrendCam.Models.ProductListItem.stock")]
+[assembly: SuppressMessage("Style", "IDE0044:Agregar modificador de solo lectura", Justification = "<pendiente>", Scope = "member", Target = "~F:PriceTrendCam.Models.ProductListItem.command")]
