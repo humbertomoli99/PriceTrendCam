312e3e5472eefa8e1ef303aadeb2e1c6a082c96a
humberto <humberto_molinalopez@hotmail.com>
2023-05-09 09:11:13 -0600

    Creando ControlPager desde template studio

---
+++ PriceTrendCam/Views/ControlPagerPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+using PriceTrendCam.ViewModels;
+
+namespace PriceTrendCam.Views;
+
+public sealed partial class ControlPagerPage : UserControl
+{
+    public ControlPagerViewModel ViewModel
+    {
+        get;
+    }
+
+    public ControlPagerPage()
+    {
+        ViewModel = App.GetService<ControlPagerViewModel>();
+        InitializeComponent();
+    }
+}
