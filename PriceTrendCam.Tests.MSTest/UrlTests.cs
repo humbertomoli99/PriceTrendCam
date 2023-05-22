c6a7c2cf92731c081bd14f72c53e832d7d72954e
humberto <humberto_molinalopez@hotmail.com>
2023-05-22 16:21:51 -0600

    añadiendo una prueba unitaria extra para LimpiarUrl

--- PriceTrendCam.Tests.MSTest/UrlTests.cs
+++ PriceTrendCam.Tests.MSTest/UrlTests.cs
@@ -59,4 +59,17 @@ public class UrlTests
         // Assert
         Assert.AreEqual("https://www.example.com/product", cleanedUrl, "The cleaned URL should not include the query string.");
     }
+
+    [TestMethod]
+    public void LimpiarUrl_UrlWithQueryString_ReturnsCleanedUrl2()
+    {
+        // Arrange
+        var urlWithQuery = "https://www.mercadolibre.com.mx/memoria-ram-fury-impact-gamer-color-negro-16gb-1-kingston-kf432s20ib16/p/MLM18931517?#reco_item_pos=1&reco_backend=machinalis-homes-pdp-boos&reco_backend_type=function&reco_client=home_navigation-recommendations&reco_id=9682a945-44b4-4232-8966-11f96f08b4e4&c_id=/home/navigation-recommendations/element&c_element_order=2&c_uid=3ec356be-c5a6-4fea-b3e8-083f62abbab2";
+
+        // Act
+        var cleanedUrl = Url.LimpiarUrl(urlWithQuery);
+
+        // Assert
+        Assert.AreEqual("https://www.mercadolibre.com.mx/memoria-ram-fury-impact-gamer-color-negro-16gb-1-kingston-kf432s20ib16/p/MLM18931517", cleanedUrl, "The cleaned URL should not include the query string.");
+    }
 }
