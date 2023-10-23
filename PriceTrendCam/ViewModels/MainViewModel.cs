8e4584b2f16fdc4060ccc1ebbf9738967a3eb46d
humberto <humberto_molinalopez@hotmail.com>
2023-10-22 20:44:40 -0600

    Revert "Fix IDE0007 Usar 'var' en lugar de un tipo explícito PriceTrendCam"
    This reverts commit 469ef079b101a5740bd944ded67d19239819c646.

--- PriceTrendCam/ViewModels/MainViewModel.cs
+++ PriceTrendCam/ViewModels/MainViewModel.cs
@@ -53,7 +53,7 @@ public partial class MainViewModel : MainModel
 
     public async Task<List<ProductInfo>> GetUnsortedProducts()
     {
-        var unsortedProducts = await App.PriceTrackerService.GetAllWithChildrenAsync<ProductInfo>();
+        List<ProductInfo> unsortedProducts = await App.PriceTrackerService.GetAllWithChildrenAsync<ProductInfo>();
         return unsortedProducts;
     }
 
